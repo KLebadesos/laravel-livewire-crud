b22a64bb4dcd08dc195bf352f2ff33b31d1204b9
Klebadesos <lebadesoskevin@gmail.com>
2020-06-20 22:14:48 +0800

    wrapper layout

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="container">
+    <h4 class="mt-5">Laravel - Livewire CRUD</h4>
+    <small>Created with <span class="fa fa-heart"></span> by Kevin Lebadesos</small>
+    @livewire('contact-component')
+  </div>
+</div>
+@endsection
